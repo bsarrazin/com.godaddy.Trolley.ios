06b45381e708c8ca0bc89bcb8fc6569d15284f29
Ben Sarrazin <b@srz.io>
2018-11-23 17:05:31 -0500

    Adding simple store

--- Trolley/Source/View Controllers/HomeViewController.swift
+++ Trolley/Source/View Controllers/HomeViewController.swift
@@ -4,23 +4,30 @@ class HomeViewController: UIViewController {
     
     // MARK: - Outlets
     @IBOutlet private(set) var tableView: UITableView!
+    @IBOutlet private(set) var priceLabel: UILabel!
     
     // MARK: - Properties
-    private(set) var cart: Cart?
+    private(set) var cart: Cart!
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.hidesBackButton = true
+        
+        if let cart = DataStore().fetch(Cart?.self) {
+            self.cart = cart
+        } else {
+            self.cart = Cart(items: .default)
+        }
+        
+        updatePrice(for: cart)
+        tableView.reloadData()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         navigationController?.setNavigationBarHidden(false, animated: true)
-        
-        cart = DataStore().fetch(Cart.self)
-        tableView.reloadData()
     }
     override func viewWillDisappear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(true, animated: true)
@@ -28,7 +35,36 @@ class HomeViewController: UIViewController {
         super.viewWillDisappear(animated)
     }
     
+    // MARK: - Segues
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "PresentStore"
+            else { return }
+        
+        let storeViewController = segue.destination as! StoreViewController
+        storeViewController.dataSource = cart.items
+        storeViewController.didSave = { [unowned self] items in
+            self.cart.items = items
+            DataStore().save(self.cart)
+            self.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Updates
+    private func updatePrice(for cart: Cart) {
+        var price: Decimal = 0.0
+        cart.items.forEach { item in
+            price += Decimal(item.quantity) * item.price
+        }
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        priceLabel.text = formatter.string(from: NSDecimalNumber(decimal: price))
+    }
+    
     // MARK: - Actions
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "PresentStore", sender: nil)
+    }
     @IBAction func signOutButtonTapped(_ sender: UIBarButtonItem) {
         let dataStore = DataStore()
         dataStore.removeAll(Cart.self)
@@ -73,6 +109,21 @@ class HomeTableViewCell: UITableViewCell {
     func configure(with item: CartItem) {
         nameLabel.text = item.name
         qtyLabel.text = "Qty: \(item.quantity)"
-        priceLabel.text = "$\(item.price)"
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        priceLabel.text = formatter.string(from: NSDecimalNumber(decimal: item.price))
+    }
+}
+
+extension Array where Element == CartItem {
+    static var `default`: [Element] {
+        return [
+            CartItem(name: "Apples", quantity: 0, price: 1),
+            CartItem(name: "Bananas", quantity: 0, price: 1.50),
+            CartItem(name: "Oranges", quantity: 0, price: 2),
+            CartItem(name: "Kiwis", quantity: 0, price: 0.50),
+            CartItem(name: "Pears", quantity: 0, price: 0.75)
+        ]
     }
 }
